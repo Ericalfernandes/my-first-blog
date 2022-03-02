42f8cc8f26be3024fc274257666c9dfbcd7b72ab
ericafernandes <elfernandes4@gmail.com>
2022-03-02 18:55:50 -0300

    Commit test, Try a new build with heroku

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 #LOCAL
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
