ac78b635fe61fe0ae1e358cc77aa23a87e08b547
Ericalfernandes <elfernandes4@gmail.com>
2022-04-24 00:32:55 -0300

    Add new urls for Comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 from django.conf import settings
@@ -7,5 +8,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #caminho, view, e nome da url
     path('post/new/', views.post_new, name='post_new'), 
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-] 
+    path('post/<int:pk>/', views.post_edit, name='post_edit'),
+    path('comment/', views.comment, name='comments'), #tentativa de exibir um html só com comentários de um post
+    path('comment/new/', views.comment_new, name='comment_new'),
+]   
