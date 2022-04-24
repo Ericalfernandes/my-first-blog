c82f20222576ce50044abecb2f69c0db42ac2f36
Ericalfernandes <elfernandes4@gmail.com>
2022-04-24 17:51:54 -0300

    Fiz url for edit post

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #caminho, view, e nome da url
     path('post/new/', views.post_new, name='post_new'), 
-    path('post/<int:pk>/', views.post_edit, name='post_edit'),
+    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
     path('comment/', views.comment, name='comments'), #tentativa de exibir um html só com comentários de um post
     path('comment/new/', views.comment_new, name='comment_new'),
 ]   
