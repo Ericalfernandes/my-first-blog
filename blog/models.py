ea69c9379750a471bf64c1c25e6dead4395d177d
Ericalfernandes <elfernandes4@gmail.com>
2022-04-24 00:30:45 -0300

    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name='comment')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+        
+    class Meta: 
+        ordering = ['created_date'] #ordenação
+
+    def create(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self): #Formatando exibição do objeto em lista
+        return 'Text: {}, Author: {}'.format(self.text, self.author)
+
+
