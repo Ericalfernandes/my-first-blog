9fcf869341e5ad4601a5eb764873c1283a3338b4
Ericalfernandes <elfernandes4@gmail.com>
2022-04-24 00:25:39 -0300

    Register and improve comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'post', 'created_date')
+    list_filter = ('post', 'created_date')
+    search_fields = ('author', 'created_date', 'text')
