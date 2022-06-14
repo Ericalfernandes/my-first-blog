490218ff5ae64fbb3b98b781f219cd28083ba022
Ericalfernandes <elfernandes4@gmail.com>
2022-06-14 18:56:52 -0300

    Creation the home route and creation the post_list route

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.home, name = 'index'),
+    path('post_list/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #caminho, view, e nome da url
     path('post/new/', views.post_new, name='post_new'), 
     path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
