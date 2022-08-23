4204eb089a7c142ba8ada32a5282704af142284d
Ericalfernandes <elfernandes4@gmail.com>
2022-08-23 19:27:24 -0300

    Changed forms, I am using widget tweaks

--- blog/forms.py
+++ blog/forms.py
@@ -5,9 +5,18 @@ from .models import Post, Comment #importa a model Post de models
 
 class PostForm(forms.ModelForm): #'PostForm' é o nome do nosso formulário para Post. E 'forms.ModelForm' usamos para informar para o django que vamos usar o padrão de formulário do proprio django
 
+    title = forms.CharField(  # defini um novo tipo de campo para text
+    required=True,
+    widget=forms.TextInput
+  )
+    text = forms.CharField(  # defini um novo tipo de campo para text
+    required=True,
+    widget=forms.Textarea
+  )
+
     class Meta: 
         model = Post #informando para o django a model que será utilizada para criar o formulário
-        fields = ('title', 'text', ) #especificação dos campos deste formulário
+        fields = ['title', 'text']  #especificação dos campos deste formulário
 
 class CommentForm(forms.ModelForm):
 
