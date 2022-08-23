9009ef738d886f6332bfc668b059b527e010872f
Ericalfernandes <elfernandes4@gmail.com>
2022-08-23 19:52:32 -0300

    WIP

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('post_list/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #caminho, view, e nome da url
     path('post/new/', views.post_new, name='post_new'), 
-    path('post/newpost/', views.form_postnew, name='form_postnew'),
     path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
     path('comment/', views.comment, name='comments'), #tentativa de exibir um html só com comentários de um post
 ]   
